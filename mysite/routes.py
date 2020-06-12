bc6536af39cc3439ea9ff23895e25210ce1f325a
Aleksander <sashabagdasarov@gmail.com>
2020-06-12 23:07:50 +0300

    second commit

--- mysite/routes.py
+++ mysite/routes.py
@@ -3,4 +3,9 @@ from mysite.views import *
 
 app.add_url_rule('/', view_func=home)
 app.add_url_rule('/about', view_func=about, methods=['GET',])
+app.add_url_rule('/cart', view_func=cart, methods=['GET', 'POST'])
+app.add_url_rule('/confirm', view_func=confirm, methods=['GET', 'POST'])
+app.add_url_rule('/login', view_func=login, methods=['GET', 'POST'])
+app.add_url_rule('/product', view_func=product, methods=['GET', 'POST'])
+app.add_url_rule('/profile', view_func=profile, methods=['GET', 'POST'])
 app.add_url_rule('/register', view_func=register, methods=['GET', 'POST'])
--- mysite/views.py
+++ mysite/views.py
@@ -9,6 +9,41 @@ def home():
     return render_template('home.html')
 
 
+def login():
+    if request.method == 'POST':
+        return redirect(url_for('home'))
+    else:
+        return render_template('login.html')
+
+
+def cart():
+    if request.method == 'POST':
+        return redirect(url_for('product'))
+    else:
+        return render_template('cart.html')
+
+
+def confirm():
+    if request.method == 'POST':
+        return redirect(url_for('home'))
+    else:
+        return render_template('confirm.html')
+
+
+def product():
+    if request.method == 'POST':
+        return redirect(url_for('home'))
+    else:
+        return render_template('product.html')
+
+
+def profile():
+    if request.method == 'POST':
+        return redirect(url_for('home'))
+    else:
+        return render_template('profile.html')
+
+
 def register():
     if request.method == 'POST':
         return redirect(url_for('home'))
